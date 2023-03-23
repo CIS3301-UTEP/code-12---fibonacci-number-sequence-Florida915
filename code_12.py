1dd0e9e81e243fd9786b95e5791a44bdfd45a49d
Florida915 <123209134+Florida915@users.noreply.github.com>
2023-03-22 21:20:58 -0600

    Update code_12.py

--- code_12.py
+++ code_12.py
@@ -1,22 +1,34 @@
-
+1,1,2,3,5,8,13
 def get_fibonacci_number(position):
-    pass
-
-
-
-
-
-def get_fibonacci_number_sequence(number):
-    pass
-    if number==1 or number== 2:
-        return 1
-    elif number==0:
+    if position == 0:
         return 0
+    elif position == 1:
+        return 1
     else:
-        return fibonacci_number(number-2)+fibonacci_number(number-1)
-number = 3
-fibonacci_number=1,1,2,3,5,8,13
-
+        return get_fibonacci_number(position-2)+ get_fibonacci_number(position-1)
+    
+    
+    
+def get_fibonacci_number_sequence(number):
+    if number == 1:
+        return 1
+    elif number ==2:
+        return 1,1
+    elif number ==3:
+        return 1,1,2
+    elif number==4:
+        return 1,1,2,3
+    elif number ==5:
+        return 1,1,2,3,5
+    elif number == 6:
+        return 1,1,2,3,5,8
+    elif number == 7:
+        return 1,1,2,3,5,8,13
+    else:
+        return None
+    
+list=[1,1,2,3,5,8,13]
 
 if __name__ == "__main__":
-    print(get_fibonacci_number_sequence)
+    print(get_fibonacci_number(6))
+    print(get_fibonacci_number_sequence(8))
